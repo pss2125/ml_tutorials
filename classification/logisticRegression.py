2b98f2f39d035e7cc6f495a70a088be2365ed2e7
pss2125 <pss2125@caa.columbia.edu>
2020-08-01 09:27:39 -0400

    logistic regression

---
+++ classification/logisticRegression.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv("Social_Network_Ads.csv")
+X = dataset.iloc[:,[2,3]].values
+y = dataset.iloc[:,4].values
+
+#splitting into train and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+#fitting logistic regression to traning set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train,y_train)
+
+#predicting test set results
+y_pred = classifier.predict(X_test)
+
+#making confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+
+#visualizing traning set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+c = np.unique(y_set)
+X1, X2 = np.meshgrid(np.arange(start=X_set[:,0].min()-1 , stop=X_set[:,0].max()+1, step = 0.01),
+                     np.arange(start= X_set[:,1].min()-1, stop=X_set[:,1].max()+1, step = 0.01))
+
+
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+              alpha = .75, cmap = ListedColormap(('red','green')))
+plt.xlim(X1.min(), X1.max())
+plt.xlim(X2.min(), X2.max())
+plt.show()
